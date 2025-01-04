9b21a1b93b824f2814036a254fdf38d2b5fc50f1
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-01-04 12:45:05 -0500

    Time: 530 ms (75.99%), Space: 20.6 MB (29.47%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        nums.sort()
+
+        for i, val in enumerate(nums):
+            if i > 0 and val == nums[i-1]:  
+                continue
+            # i>0 indicates that we are checking the next 
+            # value and val == nums[i-1] checks if the current value is same as previous 
+            # value. We only check the previous value bcoz we have sorted the array and if 
+            # duplicates existed, they would be adjacent to each other
+            
+            l,r = i+1, len(nums)-1
+            while l < r:
+                threesum = val + nums[l] + nums[r]
+                if threesum > 0:
+                    r -= 1
+                elif threesum < 0:
+                    l += 1
+                else:
+                    res.append([val, nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l-1] and l < r:
+                        l += 1
+        return res
