cdd275082eb0af02080f81e9c731df73094e415d
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-01-08 22:01:38 -0500

    Time: 99 ms (29.88%), Space: 28.7 MB (32.69%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -6,12 +6,39 @@ class Solution:
         # return nums[len(nums) - k]
 
 
+
+
+
         # Min Heap approach
         minHeap = []            # create a min heap
 
-        # process the remaining elements
+        # process the elements using heap
         for num in nums:
             heapq.heappush(minHeap, num)
             if len(minHeap) > k:
                 heappop(minHeap)
         return minHeap[0]  # The root of the heap is the kth largest element
+
+
+
+
+
+        # # Quickselect approach
+        # k = len(nums) - k   # index we are looking for
+
+        # def quickselect(l, r):
+        #     pivot, p = nums[r], l
+        #     for i in range(l,r):
+        #         if nums[i] <= pivot:
+        #             nums[p], nums[i] = nums[i], nums[p]
+        #             p += 1
+        #     nums[p], nums[r] = nums[r], nums[p]     # nums[r] = pivot
+
+        #     if p > k:
+        #         return quickselect(l, p - 1)
+        #     elif p < k:
+        #         return quickselect(p + 1, r)
+        #     else:   # p = k
+        #         return nums[p]
+
+        # return quickselect(0, len(nums) - 1)
