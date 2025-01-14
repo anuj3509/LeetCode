805484a8681c9c161f8fa214b4ac3e68257c0278
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-01-13 23:48:00 -0500

    Time: 3 ms (35.51%), Space: 18 MB (25.41%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,15 +1,32 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         
-        if len(nums) == 0:
-            return [[]]
-        
-        permutations = self.permute(nums[1:])      # subarry with all values in nums except at 1
-        res = []
+        # TC: n! * n^2
+        # SC: n * n!
 
-        for p in permutations:
-            for i in range(len(p) + 1):
-                p_copy = p.copy()
-                p_copy.insert(i, nums[0])
-                res.append(p_copy)
-        return res
+        # # Recursive Approach
+        # if len(nums) == 0:
+        #     return [[]]
+
+        # permutations = self.permute(nums[1:])      # subarry with all values in nums except at 1
+        # res = []
+        # for p in permutations:
+        #     for i in range(len(p) + 1):
+        #         p_copy = p.copy()
+        #         p_copy.insert(i, nums[0])
+        #         res.append(p_copy)
+        # return res
+
+
+        # Iterative Approach
+        permutations = [[]]     # base case
+
+        for n in nums:
+            new_permutations = []
+            for p in permutations:
+                for i in range(len(p) + 1):
+                    p_copy = p.copy()
+                    p_copy.insert(i, n)
+                    new_permutations.append(p_copy)
+            permutations = new_permutations
+        return permutations
