8a2162b074cba9a8be4778e0f545ef048b54cc2c
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-02-01 22:07:37 -0500

    Time: 40 ms (55.12%), Space: 17.7 MB (32.77%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,8 +1,15 @@
 class Solution:
     def fib(self, n: int) -> int:
 
-        # Recursive backtracking
-        if n == 0: return 0
-        if n == 1: return 1
+        # # Recursive backtracking
+        # if n == 0: return 0
+        # if n == 1: return 1
+        # return self.fib(n-1) + self.fib(n-2)
 
-        return self.fib(n-1) + self.fib(n-2)
+
+        # Dynamic Programming approach
+        if n < 2: return n
+        prev, curr = 0, 1
+        for i in range(2, n + 1):
+            prev, curr = curr, curr + prev
+        return curr
