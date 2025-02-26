5925d84323dade53f2337966dc1933a8b8715681
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-02-25 21:51:32 -0500

    Time: 247 ms (59.83%), Space: 18.2 MB (64.11%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,19 +1,34 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
 
-        # Memoization Solution [TC = SC = O(m * n)]
-        cache = {}
-        def dfs(i, a):
-            if a == amount:
-                return 1
-            if a > amount:
-                return 0
-            if i == len(coins):
-                return 0
-            if (i, a) in cache:
-                return cache[(i, a)]
+        # # Memoization Solution [TC = SC = O(m * n)]
+        # cache = {}
+        # def dfs(i, a):
+        #     if a == amount:
+        #         return 1
+        #     if a > amount:
+        #         return 0
+        #     if i == len(coins):
+        #         return 0
+        #     if (i, a) in cache:
+        #         return cache[(i, a)]
 
-            cache[(i, a)] = dfs(i, a + coins[i]) + dfs(i + 1, a)
-            return cache[(i, a)]
+        #     cache[(i, a)] = dfs(i, a + coins[i]) + dfs(i + 1, a)
+        #     return cache[(i, a)]
 
-        return dfs(0, 0)
+        # return dfs(0, 0)
+
+
+        # 2D DP cache -> TC = SC = O(m * n) ->  Suboptimal solution
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for i in range(len(coins) - 1, -1, -1):
+            nextDP = [0] * (amount + 1)
+            nextDP[0] = 1
+
+            for a in range(1, amount + 1):
+                nextDP[a] = dp[a]
+                if a - coins[i] >= 0:
+                    nextDP[a] += nextDP[a - coins[i]]
+            dp = nextDP
+        return dp[amount]
