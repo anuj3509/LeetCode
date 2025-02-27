18ee35e9b62c318286f72518180c108d8c7644ee
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-02-26 22:21:21 -0500

    Time: 121 ms (69.15%), Space: 18 MB (73.99%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,16 +1,29 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        dp = {}     # (index, curr_sum) 
-        def backtrack(i, curr_sum):
-            if (i, curr_sum) in dp:
-                return dp[(i, curr_sum)]
+        
+        # Bottom-Up approach
+        dp = [defaultdict(int) for _ in range(len(nums) + 1)]
+        dp[0][0] = 1    # 0 elements available, current sum is 0, 1 way to have that
+        for i in range(len(nums)):
+            for curr_sum, count in dp[i].items():
+                dp[i + 1][curr_sum + nums[i]] += count
+                dp[i + 1][curr_sum - nums[i]] += count
+        return dp[len(nums)][target]
+        
+        
+        
+        # # Top-Down approach with memoization
 
-            if i == len(nums):      # if i is at the end of the array
-                return 1 if curr_sum == target else 0
+        # dp = {}     # (index, curr_sum) 
+        # def backtrack(i, curr_sum):
+        #     if (i, curr_sum) in dp:
+        #         return dp[(i, curr_sum)]
+        #     if i == len(nums):      # if i is at the end of the array
+        #         return 1 if curr_sum == target else 0
 
-            dp[(i, curr_sum)] = (
-                backtrack(i + 1, curr_sum + nums[i]) + 
-                backtrack(i + 1, curr_sum - nums[i])
-            )
-            return dp[(i, curr_sum)]
-        return backtrack(0, 0)
+        #     dp[(i, curr_sum)] = (
+        #         backtrack(i + 1, curr_sum + nums[i]) + 
+        #         backtrack(i + 1, curr_sum - nums[i])
+        #     )
+        #     return dp[(i, curr_sum)]
+        # return backtrack(0, 0)
