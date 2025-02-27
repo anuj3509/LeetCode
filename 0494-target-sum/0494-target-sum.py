70955f636aa0da6fd0f27f6732c9205477bab2b1
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-02-26 22:24:33 -0500

    Time: 109 ms (76.83%), Space: 18 MB (82.59%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,14 +1,29 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         
-        # Bottom-Up approach
-        dp = [defaultdict(int) for _ in range(len(nums) + 1)]
-        dp[0][0] = 1    # 0 elements available, current sum is 0, 1 way to have that
+        
+        # Bottom-Up approach with optimised memory
+        dp = defaultdict(int)
+        dp[0] = 1    # current sum is 0 -> 1 way to have that
         for i in range(len(nums)):
-            for curr_sum, count in dp[i].items():
-                dp[i + 1][curr_sum + nums[i]] += count
-                dp[i + 1][curr_sum - nums[i]] += count
-        return dp[len(nums)][target]
+            next_dp = defaultdict(int)
+            for curr_sum, count in dp.items():
+                next_dp[curr_sum + nums[i]] += count
+                next_dp[curr_sum - nums[i]] += count
+                dp = next_dp
+        return dp[target]
+
+        
+        
+        
+        # # Bottom-Up approach
+        # dp = [defaultdict(int) for _ in range(len(nums) + 1)]
+        # dp[0][0] = 1    # 0 elements available, current sum is 0, 1 way to have that
+        # for i in range(len(nums)):
+        #     for curr_sum, count in dp[i].items():
+        #         dp[i + 1][curr_sum + nums[i]] += count
+        #         dp[i + 1][curr_sum - nums[i]] += count
+        # return dp[len(nums)][target]
         
         
         
