86a17ee0489be9ebb0524e9616d262d2009bdbe2
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-03-02 22:19:19 -0500

    Time: 28 ms (72.46%), Space: 34.5 MB (83.89%) - LeetHub

---
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.py
@@ -0,0 +1,16 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        
+        # TC: O(n) , SC: O(n)
+        less = []   # array of elements which are less
+        p = []      # pivot
+        more = []   # array of elements which are more
+
+        for n in nums:
+            if n < pivot:
+                less.append(n)
+            elif n > pivot:
+                more.append(n)
+            else:
+                p.append(n)  
+        return less + p + more
