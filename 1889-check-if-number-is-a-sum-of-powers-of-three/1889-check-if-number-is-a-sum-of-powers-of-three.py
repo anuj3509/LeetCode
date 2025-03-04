2794dc27242574240f4baf28d842db6b5048f029
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-03-03 20:20:37 -0500

    Time: 0 ms (100%), Space: 17.7 MB (59.96%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.py
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.py
@@ -1,16 +1,34 @@
 class Solution:
     def checkPowersOfThree(self, n: int) -> bool:
 
-        def backtrack(i, curr):
-            if curr == n:
-                return True
-            if curr > n or 3**i > n:    # if current sum becomes large than the number or 3^i exceeds n
+        # Math based greedy approach    ->     TC: O(log3(n))  ;   SC: O(n)
+        # Find largest power of 3^i <= n
+        i = 0
+        while 3**(i+1) <= n:
+            i += 1
+
+        # greedy, remove largest powers
+        while i >= 0:
+            power = 3**i
+            if power <= n:
+                n -= power
+            if power <= n:      # early stopping
                 return False
+            i -= 1
+        return n == 0
+        
+        
+        # # Backtracking approach
+        # def backtrack(i, curr):
+        #     if curr == n:
+        #         return True
+        #     if curr > n or 3**i > n:    # if current sum becomes large than the number or 3^i exceeds n
+        #         return False
 
-            # include case
-            if backtrack(i + 1, curr + 3**i):
-                return True
-            # skip case
-            return backtrack(i + 1, curr)
-
-        return backtrack(0, 0)
+        #     # include case
+        #     if backtrack(i + 1, curr + 3**i):
+        #         return True
+        #     # skip case
+        #     return backtrack(i + 1, curr)
+            
+        # return backtrack(0, 0)
