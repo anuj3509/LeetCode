85156febf6a48b8e82bb13ef8246d89721bcc293
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-03-06 20:09:35 -0500

    Time: 2471 ms (22.19%), Space: 32.6 MB (34.38%) - LeetHub

---
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.py
@@ -0,0 +1,31 @@
+class Solution:
+    def closestPrimes(self, left: int, right: int) -> List[int]:
+        
+        n = right + 1
+        primes = [True] * n
+        primes[0] = primes[1] = False
+
+        inf = 10 ** 10
+        best = inf
+        bestDelta = inf
+        last = inf
+
+        for k in range(2, n):
+            current = k
+            if primes[k]:
+                if left <= k <= right:
+                    if last != inf:
+                        delta = k - last
+                        if delta < bestDelta:
+                            best = last
+                            bestDelta = delta
+                    last = k
+
+                current = k * k
+                while current < n:
+                    primes[current] = False
+                    current += k
+        
+        if best == inf:
+            return [-1, -1]
+        return [best, best + bestDelta]
