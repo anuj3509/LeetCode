1b64501b3375347efa3aa1b6e20fd32955aa56dd
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-03-08 21:59:40 -0500

    Time: 769 ms (24.92%), Space: 21.2 MB (90.7%) - LeetHub

---
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
+        
+        # Sliding window approach [   TC: O(n)   ;   SC: O(n)    ]
+        n = len(colors)
+        l = 0
+        res = 0
+
+        for r in range(1, n + k - 1):
+            if colors[r % n] == colors[(r-1) % n]:      # not alternating
+                l = r                                   # we expand our window
+            if r - l + 1 > k:       # this ensures that the window is newer greater than k by 1
+                l += 1
+            if r - l + 1 == k:
+                res += 1
+        return res
