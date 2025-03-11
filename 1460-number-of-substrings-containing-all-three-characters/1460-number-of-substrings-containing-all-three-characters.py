2170c002440d1bba6d8c0a3e66e291031c778600
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-03-10 22:01:13 -0400

    Time: 109 ms (48.74%), Space: 17.9 MB (67.33%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
@@ -1,5 +1,7 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
+
+        # Using hashmap to count
         count = defaultdict(int)
         l = 0
         res = 0
@@ -14,3 +16,18 @@ class Solution:
                     count.pop(s[l])
                 l += 1
         return res
+
+
+        # # Using array to count
+        # l = 0
+        # res = 0
+        # count = [0] * 3
+
+        # for r in range(len(s)):
+        #     count[ord(s[r]) - ord("a")] += 1
+
+        #     while count[0] and count[1] and count[2]:
+        #         res += (len(s) - r)
+        #         count[ord(s[l]) - ord("a")] -= 1
+        #         l += 1
+        # return res
