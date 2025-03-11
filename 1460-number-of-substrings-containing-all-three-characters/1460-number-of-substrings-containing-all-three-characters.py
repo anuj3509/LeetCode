2dc36def3ba2c17a9886ba9036d59edf65a3a040
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-03-10 22:01:30 -0400

    Time: 91 ms (70.87%), Space: 17.9 MB (67.33%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
@@ -1,33 +1,33 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
 
-        # Using hashmap to count
-        count = defaultdict(int)
-        l = 0
-        res = 0
-
-        for r in range(len(s)):
-            count[s[r]] += 1
-
-            while len(count) == 3:
-                res += (len(s) - r)
-                count[s[l]] -= 1
-                if count[s[l]] == 0:
-                    count.pop(s[l])
-                l += 1
-        return res
-
-
-        # # Using array to count
+        # # Using hashmap to count
+        # count = defaultdict(int)
         # l = 0
         # res = 0
-        # count = [0] * 3
 
         # for r in range(len(s)):
-        #     count[ord(s[r]) - ord("a")] += 1
+        #     count[s[r]] += 1
 
-        #     while count[0] and count[1] and count[2]:
+        #     while len(count) == 3:
         #         res += (len(s) - r)
-        #         count[ord(s[l]) - ord("a")] -= 1
+        #         count[s[l]] -= 1
+        #         if count[s[l]] == 0:
+        #             count.pop(s[l])
         #         l += 1
         # return res
+
+
+        # Using array to count
+        l = 0
+        res = 0
+        count = [0] * 3
+
+        for r in range(len(s)):
+            count[ord(s[r]) - ord("a")] += 1
+
+            while count[0] and count[1] and count[2]:
+                res += (len(s) - r)
+                count[ord(s[l]) - ord("a")] -= 1
+                l += 1
+        return res
