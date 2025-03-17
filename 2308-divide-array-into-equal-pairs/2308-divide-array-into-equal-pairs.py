4a3020f6ef99022d40ac9b821d475583546571b1
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-03-16 21:11:06 -0400

    Time: 1 ms (82.71%), Space: 17.8 MB (91.41%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.py
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.py
@@ -1,12 +1,25 @@
 class Solution:
     def divideArray(self, nums: List[int]) -> bool:
-        count = {}  # hashmap
-        for num in nums:
-            if num not in count:
-                count[num] = 0
-            count[num] += 1
 
-        for n,c in count.items():
-            if c % 2:   # even count of each value
-                return False
-        return True
+        # # Using hashmap
+        # count = {}  # hashmap
+        # for num in nums:
+        #     if num not in count:
+        #         count[num] = 0
+        #     count[num] += 1
+
+        # for n,c in count.items():
+        #     if c % 2:   # even count of each value
+        #         return False
+        # return True
+
+
+        # Using hashset
+        odd_set = set()
+
+        for n in nums:
+            if n not in odd_set:
+                odd_set.add(n)
+            else:
+                odd_set.remove(n)
+        return len(odd_set) == 0
