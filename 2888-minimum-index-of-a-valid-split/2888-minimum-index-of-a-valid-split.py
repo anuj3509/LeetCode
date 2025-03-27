f998a3adc492eaf2c88e712b212270218ce73ac5
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-03-26 21:48:40 -0400

    Time: 116 ms (21.78%), Space: 34.7 MB (13.86%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.py
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.py
@@ -1,12 +1,13 @@
 class Solution:
     def minimumIndex(self, nums: List[int]) -> int:
+
+        # TC: O(n) ; SC: O(n)
         left = defaultdict(int)     # hashmap
         right = Counter(nums)
 
         for i in range(len(nums)):
             left[nums[i]] += 1
             right[nums[i]] -= 1
-
             left_len = i + 1
             right_len = len(nums) - 1 - i
 
@@ -14,3 +15,6 @@ class Solution:
             if 2 * left[nums[i]] > left_len and 2 * right[nums[i]] > right_len:
                 return i
         return -1
+
+
+        # Boyer - Moore Voting Algorithm [TC: O(n) ; SC: O(1)]
