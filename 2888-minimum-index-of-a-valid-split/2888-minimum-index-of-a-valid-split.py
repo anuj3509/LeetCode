4b0e857c34506136f1564be9084ef95ed3832f2b
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-03-26 21:55:51 -0400

    Time: 56 ms (67.33%), Space: 31.6 MB (100%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.py
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.py
@@ -1,20 +1,46 @@
 class Solution:
     def minimumIndex(self, nums: List[int]) -> int:
 
-        # TC: O(n) ; SC: O(n)
-        left = defaultdict(int)     # hashmap
-        right = Counter(nums)
+        # Boyer - Moore Voting Algorithm [TC: O(n) ; SC: O(1)]
+        majority = 0
+        count = 0
+
+        for n in nums:
+            if count == 0:
+                majority = n
+            if n == majority:
+                count += 1
+            else:
+                count -= 1
+
+        leftcount = 0
+        rightcount = nums.count(majority)
 
         for i in range(len(nums)):
-            left[nums[i]] += 1
-            right[nums[i]] -= 1
+            if nums[i] == majority:
+                leftcount += 1
+                rightcount -= 1
             left_len = i + 1
             right_len = len(nums) - 1 - i
 
             # Check for presence of majority element
-            if 2 * left[nums[i]] > left_len and 2 * right[nums[i]] > right_len:
+            if 2 * leftcount > left_len and 2 * rightcount > right_len:
                 return i
         return -1
+        
+        
+        
+        # # Using Hashmap     [TC: O(n) ; SC: O(n)]
+        # left = defaultdict(int)     # hashmap
+        # right = Counter(nums)
 
+        # for i in range(len(nums)):
+        #     left[nums[i]] += 1
+        #     right[nums[i]] -= 1
+        #     left_len = i + 1
+        #     right_len = len(nums) - 1 - i
 
-        # Boyer - Moore Voting Algorithm [TC: O(n) ; SC: O(1)]
+        #     # Check for presence of majority element
+        #     if 2 * left[nums[i]] > left_len and 2 * right[nums[i]] > right_len:
+        #         return i
+        # return -1
