ca4c93b83a84cdc6556265e3577cada6834e2907
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-03-29 20:59:44 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0768-partition-labels/0768-partition-labels.py
@@ -0,0 +1,19 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        
+        # TC: O(n)    ;   SC: O(1)
+        lastindex = {}      # char : last index in s
+
+        # building our hashmap to map each character to its last index in the string
+        for i, c in enumerate(s):
+            lastindex[c] = i
+
+        res = []
+        size, end = 0, 0
+        for i, c in enumerate(s):
+            size += 1
+            end = max(end, lastindex[c])
+            if i == end:
+                res.appedn(size)
+                size = 0
+        return res
