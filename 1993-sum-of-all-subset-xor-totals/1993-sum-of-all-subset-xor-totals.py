bbbe2b71e6b4c96a4bdcee5321a36ba396afdccc
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-04-05 15:53:26 -0400

    Time: 0 ms (100%), Space: 17.6 MB (82.84%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.py
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.py
@@ -1,11 +1,18 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
         
-        def dfs(i, total):
-            
-            if i == len(nums):
-                return total
-            return (dfs(i + 1, total ^ nums[i]) + dfs(i + 1, total))    
-                    # include nums[i]               # not include nums[i]
+        # # TC: O(2^n)
+        # def dfs(i, total):
+        #     if i == len(nums):
+        #         return total
+        #     return (dfs(i + 1, total ^ nums[i]) + dfs(i + 1, total))    
+        #             # include nums[i]               # not include nums[i]
 
-        return dfs(0, 0)
+        # return dfs(0, 0)
+
+
+        # TC: O(n)
+        res = 0
+        for n in nums:
+            res = res | n   # OR operator
+        return res * 2**(len(nums) - 1)     # res shifted to left by (len(nums) - 1)
