32340f6a6c7b0a9eff73d55e14deaae1a8cdd16c
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-04-07 23:09:01 -0400

    Time: 4 ms (51.86%), Space: 17.8 MB (73.67%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.py
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.py
@@ -2,17 +2,23 @@ class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
         n = len(nums)
         d = {}
+
+        # Build frequency dictionary
         for x in nums:
             d[x] = d.get(x, 0) + 1
         dup = 0
+
+        # Count numbers that appear more than once
         for v in d.values():
             if v > 1:
                 dup += 1
+        
         if dup == 0:
             res = 0
         else:
             op = 0
             i = 0
+            # Remove elements in blocks of 3 until all remaining are distinct
             while i < n:
                 for _ in range(3):
                     if i >= n:
