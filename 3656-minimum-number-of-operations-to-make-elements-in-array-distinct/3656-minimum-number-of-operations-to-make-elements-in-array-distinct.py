80465733f6423337d5440cac14ba24837ef44794
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-04-07 23:13:29 -0400

    Time: 0 ms (100%), Space: 17.9 MB (27.03%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.py
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.py
@@ -1,35 +1,48 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        n = len(nums)
-        d = {}
 
-        # Build frequency dictionary
-        for x in nums:
-            d[x] = d.get(x, 0) + 1
-        dup = 0
+        visited = set()
+        for i in range(len(nums) - 1,  -1, -1):
+            if nums[i] in visited:
+                return i // 3 + 1
+            visited.add(nums[i])
+        return 0
 
-        # Count numbers that appear more than once
-        for v in d.values():
-            if v > 1:
-                dup += 1
+
+
+
+        # # TC = SC = O(n)
+
+        # n = len(nums)
+        # d = {}
+
+        # # Build frequency dictionary
+        # for x in nums:
+        #     d[x] = d.get(x, 0) + 1
+        # dup = 0
+
+        # # Count numbers that appear more than once
+        # for v in d.values():
+        #     if v > 1:
+        #         dup += 1
         
-        if dup == 0:
-            res = 0
-        else:
-            op = 0
-            i = 0
-            # Remove elements in blocks of 3 until all remaining are distinct
-            while i < n:
-                for _ in range(3):
-                    if i >= n:
-                        break
-                    x = nums[i]
-                    d[x] -= 1
-                    if d[x] == 1:
-                        dup -= 1
-                    i += 1
-                op += 1
-                if dup == 0:
-                    break
-            res = op
-        return res
+        # if dup == 0:
+        #     res = 0
+        # else:
+        #     op = 0
+        #     i = 0
+        #     # Remove elements in blocks of 3 until all remaining are distinct
+        #     while i < n:
+        #         for _ in range(3):
+        #             if i >= n:
+        #                 break
+        #             x = nums[i]
+        #             d[x] -= 1
+        #             if d[x] == 1:
+        #                 dup -= 1
+        #             i += 1
+        #         op += 1
+        #         if dup == 0:
+        #             break
+        #     res = op
+        # return res
