e7c01a57d2a8c8e367b2aa5f62dd80b443658c69
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-05-26 20:46:54 -0400

    Time: 0 ms (100%), Space: 17.7 MB (74.3%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,3 +1,16 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x ** n
+
+        # TC: O(log2(n))
+        def helper(x, n):   # this function also avoids passing -ve exponent
+            if n == 0:
+                return 1
+            if x == 0:
+                return 0
+        
+            res = helper(x, n // 2)
+            res = res * res
+            return x * res if n % 2 else res    # return (x * x^2 * x^2) if exponent is -ve
+
+        res = helper(x, abs(n))
+        return res if n >= 0 else 1/res
