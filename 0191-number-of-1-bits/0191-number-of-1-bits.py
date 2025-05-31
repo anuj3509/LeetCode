4e500d60e10f2b48448661945c5c68d892165f1c
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-05-30 22:34:41 -0400

    Time: 0 ms (100%), Space: 17.9 MB (10.19%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -2,6 +2,8 @@ class Solution:
     def hammingWeight(self, n: int) -> int:
         res = 0
         while n:
-            res += n % 2
-            n = n >> 1      # bit shift it to the right
+            n &= (n-1)
+            res += 1
+            # res += n % 2
+            # n = n >> 1      # bit shift it to the right
         return res
