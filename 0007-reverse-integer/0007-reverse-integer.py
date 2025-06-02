3502e74557731e802df0028aaafe0bc199f8ce31
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-06-02 18:37:58 -0400

    Time: 43 ms (31.9%), Space: 17.7 MB (82.45%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,10 +1,15 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        org = x
-        x = abs(x)
-        res = int(str(x)[::-1])
+        org = x     # holds the original value including its sign
+        x = abs(x)  # using absolute value to simplify digit reversal
+        res = int(str(x)[::-1])     # converts to string, reverses the characters, then back to int
+
+        # if the original was negative, restore the sign by multiplying with -1
         if org < 0:
             res *= -1
-        if res < -(1 << 31) or res > (1 << 31) - 1:
+
+        # valid range is [1 * −2^31, 1 * 2^31 - 1]      -->      [−(1<<31), (1<<31)−1]
+        # check for 32‑bit signed integer overflow
+        if res < -(1 << 31) or res > (1 << 31) - 1:     
             return 0
         return res
