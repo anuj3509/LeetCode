cc9b58a1222f9c8eb88a061c5bbf5621d412db9d
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-06-17 16:16:31 -0400

    Time: 320 ms (9.39%), Space: 25.4 MB (26.62%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,34 +1,33 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        visited = set()     # tro track the visited portion
+        islands = 0
+        
+        # empty grid
         if not grid:
             return 0
-
-        rows, cols = len(grid), len(grid[0])
-        visited = set()   # to track the visited positions
-        islands = 0
-
+        
         def bfs(r, c):
             q = collections.deque()
-            visited.add((r, c))
             q.append((r, c))
+            visited.add((r, c))
 
             while q:
                 row, col = q.popleft()
-                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-
+                directions = [[1, 0], [-1, 0],[0, 1],[0, -1]]
                 for dr, dc in directions:
-                    r, c = row + dr, col + dc
-                    if ((r) in range(rows) and
-                        (c) in range(cols) and
-                        grid[r][c] == "1" and     # this position is a land position
-                        (r, c) not in visited):
+                    r = row + dr
+                    c = col + dc
+                    if (r in range(rows) and c in range(cols) and 
+                        grid[r][c] == "1" and (r, c) not in visited):   # this is land
                         q.append((r, c))
                         visited.add((r, c))
-
-
+        
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1" and (r, c) not in visited:
                     bfs(r, c)
                     islands += 1
+
         return islands
