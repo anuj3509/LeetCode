faa79d75413f0bd270d8cf393e0856df8f2479e5
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-06-22 18:18:39 -0400

    Time: 3 ms (90.96%), Space: 21.2 MB (79.88%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,17 +1,33 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        countmap = {}
+        # countmap = {}
+        # for n in nums:
+        #     countmap[n] = 1 + countmap.get(n,0) #create a hashmap for the input array
 
-        for n in nums:
-            countmap[n] = 1 + countmap.get(n,0) #create a hashmap for the input array
+        # arr = []
+        # for n,count in countmap.items():
+        #     arr.append([count,n])
+        # arr.sort()
 
-        arr = []
-        for n,count in countmap.items():
-            arr.append([count,n])
-        arr.sort()
+        # result = []
+        # while len(result) < k:
+        #     result.append(arr.pop()[1])
 
-        result = []
-        while len(result) < k:
-            result.append(arr.pop()[1])
+        # return result
+
+
+        count = {}
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
         
-        return result
+        heap = []
+        for num in count.keys():
+            heapq.heappush(heap, (count[num], num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
+        res = []
+        for i in range(k):
+            res.append(heapq.heappop(heap)[1])
+        
+        return res
