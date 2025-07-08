7631fd5355723bec419cedc0dd5fccca1ae04707
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-07-08 14:34:28 -0400

    Time: 0 ms (100%), Space: 17.8 MB (35.07%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.py
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.py
@@ -1,7 +1,31 @@
 class Solution:
     def kthCharacter(self, k: int) -> str:
-        word = "a"
-        while len(word) < k:
-            nxt = ''.join(chr(ord(c)+1) for c in word)
-            word += nxt
-        return word[k-1]
+
+        cnt = 0
+        length = 1
+        # compute the number of expansions - find the smallest power of 2 >= k
+        while length < k:
+            length *= 2
+            cnt += 1
+
+        ans = 0  # number of increments, i.e., how many times we move to second half
+        while cnt > 0:
+            if k > length // 2:
+                ans += 1
+                k -= length // 2
+            length //= 2
+            cnt -= 1
+        return chr(ord('a') + ans)
+
+        # TC: O(log k), SC: O(1)
+
+
+        # Brute Force
+        
+        # word = "a"
+        # while len(word) < k:
+        #     nxt = ''.join(chr(ord(c)+1) for c in word)
+        #     word += nxt
+        # return word[k-1]
+
+        # # TC = SC = O(k)
