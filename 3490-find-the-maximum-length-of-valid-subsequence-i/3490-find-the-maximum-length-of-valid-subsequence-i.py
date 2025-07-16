21a07fcd700902269b85bf0ff75cd33094732afd
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-07-15 21:18:07 -0400

    Time: 43 ms (90.7%), Space: 39.2 MB (33.33%) - LeetHub

---
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maximumLength(self, nums: List[int]) -> int:
+        odd, even = 0, 0
+        odd_even = 0    # length of longest subsequence ending in odd after an even
+        even_odd = 0    # length of longest subsequence ending in even after an odd
+        for num in nums:
+            if num % 2:
+                odd += 1
+                odd_even = even_odd + 1 
+            else:
+                even += 1
+                even_odd = odd_even + 1
+        return max(odd, even, odd_even, even_odd)
