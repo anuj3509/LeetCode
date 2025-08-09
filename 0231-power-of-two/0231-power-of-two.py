49d54b6532b71cd8fb1a2d3c747286fec2576542
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-08-08 21:08:14 -0400

    Time: 0 ms (100%), Space: 17.6 MB (83.5%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -2,3 +2,7 @@ class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         if n == 0: return False
         return  n & (n-1) == 0
+
+        # idea: if any number is a power of 2, only 1 digit in its binary representation would be '1',
+        # rest all would be '0'. So taking logical and of the number and the number before would 
+        # result in 0, which will only happen if a number is a power of 2
