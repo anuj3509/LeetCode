23b75ca93e1e314115ea5399332ba82ca33d8ae7
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-08-10 16:17:02 -0400

    Time: 4 ms (30.64%), Space: 18.2 MB (26.67%) - LeetHub

---
+++ 2331-intersection-of-multiple-arrays/2331-intersection-of-multiple-arrays.py
@@ -0,0 +1,14 @@
+class Solution:
+    def intersection(self, nums: List[List[int]]) -> List[int]:
+        res = []
+        numOfArrays = len(nums)   # number of arrays in nums
+        digit_map = {}
+        for subarr in nums:
+            for digit in subarr:
+                digit_map[digit] = 1 + digit_map.get(digit,0)
+        
+        for digit in digit_map:
+            if digit_map.get(digit,0) == numOfArrays:
+                res.append(digit)
+                
+        return sorted(res)
