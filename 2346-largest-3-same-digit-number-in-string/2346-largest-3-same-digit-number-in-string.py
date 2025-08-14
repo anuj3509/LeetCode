180ec9e43e3a625abf8b4128d30b74d534b73948
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-08-13 20:27:44 -0400

    Time: 7 ms (14.75%), Space: 17.9 MB (39.13%) - LeetHub

--- 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.py
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.py
@@ -5,7 +5,7 @@ class Solution:
 
         for r in range(2, len(num)):
             substring = num[l] + num[r-1] + num[r]
-            if int(substring) % 111 == 0:
+            if int(substring) % 111 == 0:   # if all the digits are equal, then 111 would be a factor
                 res.append(substring)
             l += 1
         return max(res) if res else ""
