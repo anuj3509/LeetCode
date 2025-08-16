2ddb674c63f3b68b8f8471ad0c584dcfd5346ed3
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-08-16 19:40:01 -0400

    Time: 453 ms (75.44%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -3,5 +3,5 @@
 select w1.id        -- you can also use id of w2, both are same
 from Weather w1
 inner join Weather w2
-where datediff(w1.recordDate, w2.recordDate) = 1    -- calculates diff in dates ; you can also use ON instead od where
+on datediff(w1.recordDate, w2.recordDate) = 1    -- calculates diff in dates ; you can also use ON instead od where
 and w1.temperature > w2.temperature
