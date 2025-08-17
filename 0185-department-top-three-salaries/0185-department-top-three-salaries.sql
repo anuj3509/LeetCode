2d685a8c556f9f4b04dca3d37efc8c2cd8b98ae8
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-08-17 11:10:51 -0400

    Time: 1324 ms (20.72%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -5,8 +5,8 @@ select d.name as Department,
     e1.salary as Salary
 from Employee e1 inner join Department d
 on e1.departmentId = d.id
-where 3 > (
+where (
     select count(distinct (e2.salary))
     from Employee e2
     where e2.salary > e1.salary and e1.departmentId = e2.departmentId
-)
+) < 3
