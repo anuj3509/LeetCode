55b6c9a0786c521134847886a83f8ba14865bd73
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-08-19 20:32:22 -0400

    Time: 57 ms (64.22%), Space: 20.3 MB (74.47%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.py
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.py
@@ -10,7 +10,12 @@ class Solution:
                     if i == 0 or j == 0:
                         dp[i][j] = 1  # first row/col can only form 1x1 square
                     else:
+                        # if matrix[i][j] == 1, the size of the largest square ending at (i, j) 
+                        # depends on its neighbors, i.e.,  dp[i][j] = 1 + min(top, left, top-left) if matrix[i][j] == 1
                         dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
                     total += dp[i][j]
 
         return total
+
+
+        # TC = SC = O(rows x cols)
