181ac548af6ac27f6c21020b1e2c4ad3930c0e14
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-08-25 10:24:42 -0400

    Time: 0 ms (100%), Space: 17.6 MB (85%) - LeetHub

--- 3995-gcd-of-odd-and-even-sums/3995-gcd-of-odd-and-even-sums.py
+++ 3995-gcd-of-odd-and-even-sums/3995-gcd-of-odd-and-even-sums.py
@@ -1,3 +1,10 @@
 class Solution:
     def gcdOfOddEvenSums(self, n: int) -> int:
         return n
+
+        # idea !! 
+
+        # sum of first 'n' odd numbers = n^2
+        # sum of first 'n' odd numbers = n(n+1)
+
+        # thus GCD( n^2, n(n+1) ) = n
