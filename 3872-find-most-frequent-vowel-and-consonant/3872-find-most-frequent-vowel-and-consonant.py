3cbc82594f2fc5bea783c5a103604763cf0e332b
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-09-13 16:51:32 -0400

    Time: 3 ms (66.07%), Space: 17.9 MB (38.62%) - LeetHub

---
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxFreqSum(self, s: str) -> int:
+        
+        def isVowel(ch):
+            str = "aeiouAEIOU"
+            return (str.find(ch) != -1)
+
+        max_vowel_count = max_consonants_count = 0
+        char_map = {}
+
+        for char in s:
+            char_map[char] = 1 + char_map.get(char, 0)
+
+        for key in char_map.keys():
+            if isVowel(key):
+                max_vowel_count = max(max_vowel_count, char_map.get(key))
+            else:
+                max_consonants_count = max(max_consonants_count, char_map.get(key))
+        
+        print(max_vowel_count, max_consonants_count)
+        return max_vowel_count + max_consonants_count
