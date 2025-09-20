8a076be5e2041ac4949e21bcf8be3768c7ee5924
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-09-20 19:33:29 -0400

    Time: 3 ms (67.78%), Space: 18 MB (9.13%) - LeetHub

--- 3997-maximize-sum-of-at-most-k-distinct-elements/3997-maximize-sum-of-at-most-k-distinct-elements.py
+++ 3997-maximize-sum-of-at-most-k-distinct-elements/3997-maximize-sum-of-at-most-k-distinct-elements.py
@@ -1,8 +1,10 @@
 class Solution:
     def maxKDistinct(self, nums: List[int], k: int) -> List[int]:
-        numSet = set(nums)
-
+        # numSet = set(nums)
         # while len(numSet) > k:
         #     numSet.remove(min(numSet))
+        # return sorted((list(numSet)), reverse = True)
 
-        return sorted((list(numSet)), reverse = True)[:k]
+
+        # ----------- Single line approach -----------
+        return sorted((list(set(nums))), reverse = True)[:k]
