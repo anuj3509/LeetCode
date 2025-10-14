6b568ac617e50880c638197906b92be4f255e03d
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-10-14 16:25:53 -0400

    Time: 80 ms (75.27%), Space: 17.9 MB (45.16%) - LeetHub

--- 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.py
+++ 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.py
@@ -3,16 +3,16 @@ class Solution:
         if k == 1:
             return True
 
-        size = 0
+        size = 1
         for i in range(1, len(nums) - k):
             j = i + k
             if nums[i] > nums[i-1] and nums[j] > nums[j-1]:
                 size += 1
             else:
-                size = 0
+                size = 1
             j += 1
 
-            if size == k-1:
+            if size == k:
                 return True
 
         return False      
