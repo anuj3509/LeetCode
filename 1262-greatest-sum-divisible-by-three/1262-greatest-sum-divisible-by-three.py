8f3ad3891a2b1c84660ecdef52947e89bb85fcca
Anuj Patel <157984808+anuj3509@users.noreply.github.com>
2025-11-23 12:47:14 -0500

    Time: 27 ms (66.57%), Space: 21.6 MB (99.7%) - LeetHub

---
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxSumDivThree(self, nums: List[int]) -> int:
+        total = 0        
+        smallest_one = smallest_two = float("inf")  # sum of numbers
+
+        for n in nums:
+            total += n
+            if n % 3 == 1:
+                smallest_two = min(smallest_two, smallest_one + n)
+                smallest_one = min(smallest_one, n)
+
+            if n % 3 == 2:
+                smallest_one = min(smallest_one, smallest_two + n)
+                smallest_two = min(smallest_two, n)
+        
+        if total % 3 == 0:
+            return total
+        if total % 3 == 1:
+            return total - smallest_one
+        if total % 3 == 2:
+            return total - smallest_two
